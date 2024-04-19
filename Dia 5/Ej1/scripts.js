5690df6183017d6e14a96ba543806e599c4ab363
Oli <oliverlopez42069@gmail.com>
2024-04-19 06:28:28 -0500

    feat: :hammer: Crear procesamiento de entrada y ubicación Dia5
    Crear procesamiento de entrada del usuario para obtención de la ubicación, se construye la tabla y se ubica la reina en el dia 5.

--- Dia 5/Ej1/scripts.js
+++ Dia 5/Ej1/scripts.js
@@ -6,5 +6,17 @@ const rl = readline.createInterface({
 });
 
 rl.question('Posición de la primera reina (fila columna): ', (position) => {
+    const [row, col] = position.split(' ').map(Number);
+
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      board.push(Array(8).fill('O'));
+    }
+  
+    board[row - 1][col - 1] = 'R';
+  
+    console.log('Tablero de ajedrez:');
+    board.forEach(row => console.log(row.join(' ')));
+  
   rl.close();
 });
