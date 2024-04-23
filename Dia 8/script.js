6ae4f3dfaf5b076179fe41a35be64051e154f867
Oli <oliverlopez42069@gmail.com>
2024-04-23 08:05:53 -0500

    ci: :hammer: Importar datos del api Dia8
    Se importaron los datos del api de darth vader en el dia 8

--- Dia 8/script.js
+++ Dia 8/script.js
@@ -21,3 +21,94 @@ fetch(request).then((Response)  => {
     console.log(data);
     p.innerHTML = JSON.stringify(data.mass);
 })
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text4");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.hair_color);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text5");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.skin_color);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text6");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.eye_color);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text7");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.birth_year);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text8");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.gender);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text9");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.homeworld);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text10");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.films);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text11");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.species);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text12");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.vehicles);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text13");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.starships);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text14");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.created);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text15");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.edited);
+})
+fetch(request).then((Response)  => {
+    return Response.json();
+}).then((data) => {
+    let p = document.getElementById("text16");
+    console.log(data);
+    p.innerHTML = JSON.stringify(data.url);
+})
