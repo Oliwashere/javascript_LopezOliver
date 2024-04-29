9df4f47ef4e9600b9775a245b0fb0e05829e3b0e
Oli <oliverlopez42069@gmail.com>
2024-04-28 20:21:37 -0500

    Revert "feat: :hammer: Llamar a las variables al script Dia11"
    This reverts commit 14dc8b4f6db0d8cf3fb4d8c414e715baed7d5fad.

--- Dia 11/js/script.js
+++
@@ -1,7 +0,0 @@
-const pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('.pokemon_id');
-const pokemonImage = document.querySelector('.pokemon_img');
-const form = document.querySelector('.form');
-const input = document.querySelector('.input__search');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-next');
