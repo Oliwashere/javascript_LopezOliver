43a165fe06c360662daebc2254d02fab2472a2b5
Oli <oliverlopez42069@gmail.com>
2024-04-29 21:25:41 -0500

    feat: :hammer: Repartir cartas Dia12
    Se creó la funcionabilidad de repartir las cartas al jugador y al dealer en el dia 12

--- Dia 12/js/script.js
+++ Dia 12/js/script.js
@@ -11,3 +11,14 @@ function initDeck() {
 window.onload = function() {
     initDeck();
 }
+
+function dealInitialCards() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=4`)
+        .then(response => response.json())
+        .then(data => {
+            const playerCards = [data.cards[0], data.cards[2]];
+            const dealerCards = [data.cards[1], data.cards[3]];
+            console.log('Cartas del jugador:', playerCards);
+            console.log('Cartas del crupier:', dealerCards);
+        })
+}
