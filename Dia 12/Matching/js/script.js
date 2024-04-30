bfe4ef24d135ca9b7803ba33afe379e7b0f702c6
Oli <oliverlopez42069@gmail.com>
2024-04-30 08:50:46 -0500

    feat: :hammer: Seleccionar cartas Dia12
    Se implementó la funcionalidad de barajear las cartas y repartirlas en el dia 12

--- Dia 12/Matching/js/script.js
+++ Dia 12/Matching/js/script.js
@@ -5,4 +5,13 @@ async function startGame() {
     const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
     const data = await response.json();
     deckId = data.deck_id;
+    await drawCards();
+}
+
+async function drawCards() {
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=32`);
+    const data = await response.json();
+    const drawnCards = data.cards.slice(0, 16);
+    cards = [...drawnCards, ...drawnCards];
+    shuffleArray(cards);
 }
