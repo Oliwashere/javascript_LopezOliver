e2c34494304b31f012ea74b302ba6984a891bff8
Oli <oliverlopez42069@gmail.com>
2024-05-03 08:27:40 -0500

    feat: :hammer: Crear funcionalidad mostrar Dia13 QUIZ
    Se implementó la funcionalidad de mostrar el contenido registrado en el dia 13

--- Dia 13 - Quiz/js/script.js
+++ Dia 13 - Quiz/js/script.js
@@ -2,5 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('crud-form');
     const dataContainer = document.getElementById('data-container');
     let data = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : [];
+
+    //Mostrar
+    function renderData() {
+      dataContainer.innerHTML = '';
+      data.forEach(function(item, index) {
+        const row = `
+          <div class="data-row">
+            <span> Nombre Personaje: ${item.heroname} <br>
+             Nombre Actor: ${item.actorname} <br>
+             Edad Actor: ${item.actorage} <br>
+             Ubicación: ${item.location} <br>
+             Poster: ${item.poster} <br>    
+             Fecha: ${item.date} <br>
+             Productora: ${item.producer}</span>
+            <button onclick="editData(${index})">Editar</button>
+            <button onclick="deleteData(${index})">Eliminar</button>
+          </div>
+        `;
+        dataContainer.innerHTML += row;
+      });
+    }
 });
   
